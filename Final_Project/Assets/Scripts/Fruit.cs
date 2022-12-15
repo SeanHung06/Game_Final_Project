7b6aec1490b2dbaad8ced6755d092d000e7c5bc8
洪立軒 <u8351574@gmail.com>
2022-12-15 17:24:19 -0500

    move the Test Sean_Gamemanager to Game_manager gile

--- Final_Project/Assets/Scripts/Fruit.cs
+++ Final_Project/Assets/Scripts/Fruit.cs
@@ -6,23 +6,23 @@ public class Fruit : MonoBehaviour
 {
     internal string fruitName;
     private void OnCollisionEnter(Collision other) {
-        Debug.Log(Sean_GameManager1.target_fruit_name);
-        Debug.Log(transform.gameObject.name);
+        //Debug.Log(GameManager.target_fruit_name);
+        //Debug.Log(transform.gameObject.name);
 
-        if(other.transform.name == "Player" && transform.gameObject.name == (Sean_GameManager1.target_fruit_name+"(Clone)")){
+        if(other.transform.name == "Player" && transform.gameObject.name == (GameManager.target_fruit_name+"(Clone)")){
             // [Sean] Add the Score Update if we encounter the correct Fruit
             Debug.Log("The player pickup "  +  transform.gameObject.name);
-            Sean_GameManager1.is_target_fruit = true;
-            Sean_GameManager1.Score += 300;
-            Debug.Log(Sean_GameManager1.Score);
+            GameManager.is_target_fruit = true;
+            GameManager.Score += 300;
+            Debug.Log(GameManager.Score);
             Object.Destroy(transform.gameObject);
             // [Sean]
         }
         else{
             // [Sean] Minus point if we encounter the wrong fruit 
             // [Sean] Add the warning when player pick up the wrong fruit
-            Sean_GameManager1.is_wrong_fruit = true;
-            Sean_GameManager1.Score -= 100;
+            GameManager.is_wrong_fruit = true;
+            GameManager.Score -= 100;
             Object.Destroy(transform.gameObject);
             //Debug.Log(other.transform.name);
         }
