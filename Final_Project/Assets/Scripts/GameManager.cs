a19ff6b3e87ad625fd73528a650310d95fcf6dd9
Joshua Huang <joshua@vl965-172-31-41-172.wireless.umass.edu>
2022-12-18 13:55:15 -0500

    Merge testmanager into gamemanager

--- Final_Project/Assets/Scripts/GameManager.cs
+++ Final_Project/Assets/Scripts/GameManager.cs
@@ -39,6 +39,15 @@ public class GameManager : MonoBehaviour
     public GameObject text_box;
     private float timestamp_last_msg = 0.0f; // timestamp used to record when last message on GUI happened (after 7 sec, default msg appears)
 
+    //[Joshua]Create maze and begin the game
+    public int TargetNumber = 10;
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+    public GameObject playerPrefab;
+	private GameObject playerInstance;
+    public FruitManager FruitManagerPrefab;
+    private FruitManager FruitManagerInstance;
+
     // [Sean]
     void Start()
     {
@@ -47,10 +56,10 @@ public class GameManager : MonoBehaviour
 
         //Spawn player
         //player_prefab = (GameObject)Resources.Load(@"Standard Assets/Characters/FirstPersonCharacter/Prefabs/FPSController");
-        player_prefab = PlayerPerfab;
-        if(player_prefab == null) Debug.Log("Wrong Path");
-        else SpawnGameobject(player_prefab, "Player", new Vector3(0.0f, 1.0f, 0.0f));
-
+        //player_prefab = PlayerPerfab;
+        //if(player_prefab == null) Debug.Log("Wrong Path");
+        //else SpawnGameobject(player_prefab, "Player", new Vector3(0.0f, 1.0f, 0.0f));
+        StartCoroutine(BeginGame());
 
         // [Sean]Select the target fruit for player to find 
         GameObject target_fruit = GetRamdonFruitId();
@@ -128,6 +137,64 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
+    private IEnumerator BeginGame () {
+        
+        //[Joshua]Create Maze
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        yield return StartCoroutine(mazeInstance.Generate());
+        
+        //[Johsua]Get random maze grid for player and target fruits
+        List<MazeCell> randomCells = GetRandomCellArray(TargetNumber+1);
+        Debug.Log(randomCells.Count);
+        
+
+        //Create Player instance
+        playerInstance = Instantiate(playerPrefab);
+        Vector3 playerPos = randomCells[randomCells.Count-1].transform.position;
+        randomCells.RemoveAt(randomCells.Count-1);
+        playerPos.y = 10f;
+        playerInstance.transform.localPosition = playerPos;
+        playerInstance.transform.localScale = new Vector3(1f, 0.4f, 1f);
+        playerInstance.transform.name = "Player";
+        CapsuleCollider collider = playerInstance.GetComponent<CapsuleCollider>();
+        collider.radius = 0.38f;
+
+        //Create Fruit Manager Instance
+        FruitManagerInstance = Instantiate(FruitManagerPrefab) as FruitManager;
+        FruitManagerInstance.InitializeTarget();
+        //Create Target Fruits
+        while(randomCells.Count>0){
+            Vector3 pos = randomCells[randomCells.Count-1].transform.position;
+            randomCells.RemoveAt(randomCells.Count-1);
+            // [Sean] Add to Randomize the maze fruit 
+            FruitManagerInstance.InitializeTarget();
+            // [Sean]
+            FruitManagerInstance.CreateTargetFruit(pos);
+        }
+    }
+
+	private void RestartGame () {
+        StopAllCoroutines();
+        Object.Destroy(mazeInstance.gameObject);
+        if (playerInstance != null) {
+			Object.Destroy(playerInstance.gameObject);
+		}
+        Object.Destroy(FruitManagerInstance.gameObject);
+        StartCoroutine(BeginGame());
+    }
+    private List<MazeCell> GetRandomCellArray(int number){
+        List<MazeCell> res = new List<MazeCell>();
+        int[,] mem = new int [mazeInstance.size.x, mazeInstance.size.z];
+        while(res.Count<number){
+            IntVector2 coor = mazeInstance.RandomCoordinates;
+            if(mem[coor.x, coor.z]==1)continue;
+            res.Add(mazeInstance.GetCell(coor));
+            mem[coor.x, coor.z] = 1;
+        }
+        return res;
+    }
+
     private GameObject SpawnGameobject(GameObject prefab, string objName, Vector3 pos){
         GameObject obj = Instantiate(prefab);
         obj.name = objName;
